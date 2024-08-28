70c66dd06c0de0aa430a71e8f7a6ede6f4c44b09
Niyaj Kumanali <niyajkumanali@gmail.com>
2024-08-28 15:06:26 +0530

    Code updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "",
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173, // Your desired port
+  },
 })
