aecdc2632560fd127c8da55982b22d678f4849fc
DESKTOP-TB3I824\admin <sainaath4763@gmail.com>
2024-09-06 16:34:20 +0530

    code for email

--- frontend/src/api/MongoAPIInstance.ts
+++ frontend/src/api/MongoAPIInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const mongoAPI: AxiosInstance = axios.create({
-  baseURL: 'http://3.111.205.170:2000',
+  baseURL: 'http://localhost:2000',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -11,5 +11,7 @@ export const getAllWarehouseByUserId = async (userId: string) => {
   const response = await mongoAPI.get(`/warehouse/getallwarehouse/${userId}`);
   return response;
 };
+
+
 
 // 3.111.205.170
