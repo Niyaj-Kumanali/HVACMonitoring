e9bd74410a17813a14c3a86e6bc53d2fdd23fb81
Niyaj Kumanali <niyajkumanali@gmail.com>
2024-09-18 02:14:50 +0530

    Updated the dashboard to get correct data over the interval and solved the unnecessory rendering

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import inspect from 'vite-plugin-inspect'; // Import the inspect plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    inspect(), // Add the inspect plugin
+  ],
   optimizeDeps: {
-    include: ['date-fns']
-  }
+    include: ['date-fns'],
+  },
 });
