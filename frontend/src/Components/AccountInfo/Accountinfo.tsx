1abf3daef054bfde127e2036c621fd31d49f55d4
Niyaj Kumanali <niyajkumanali@gmail.com>
2024-09-30 12:23:13 +0530

    updates

--- frontend/src/Components/AccountInfo/Accountinfo.tsx
+++ frontend/src/Components/AccountInfo/Accountinfo.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import SaveIcon from '@mui/icons-material/Save';
@@ -10,72 +10,95 @@ import Loader from '../Loader/Loader';
 import { User } from '../../types/thingsboardTypes';
 import CustomSnackBar from '../SnackBar/SnackBar';
 import { useNavigate } from 'react-router-dom';
-import React from 'react';
 
 const Accountinfo = () => {
-  const [currentUser, setCurrentUser] = useState<User>({});
-  const [loading, setLoading] = useState<boolean>(false);
-  const [loader, setLoader] = useState<boolean>(true);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [loadingSave, setLoadingSave] = useState(false);
+  const [loader, setLoader] = useState(true);
   const [open, setOpen] = useState(false);
   const [message, setMessage] = useState('');
-  const [snackbarType, setSnackbarType] = useState('success');
+  const [snackbarType, setSnackbarType] = useState<'success' | 'error'>(
+    'success'
+  );
   const navigate = useNavigate();
-  console.log("accountinfo")
+
   useEffect(() => {
     const fetchCurrentUser = async () => {
       try {
         const response = await getCurrentUser();
         setCurrentUser(response.data);
-      } catch (err:any) {
-        console.log('Failed to load user', err);
+      } catch (err: any) {
+        console.error('Failed to load user', err);
         setSnackbarType('error');
         if (err.status === 401) {
-          setMessage('Session has expired navigating to login page');
+          setMessage('Session has expired. Redirecting to login page.');
           setOpen(true);
           setTimeout(() => {
             navigate('/login');
           }, 2000);
-        } 
+        }
       } finally {
-        setTimeout(() => {
-          setLoader(false);
-        }, 700);
+        setLoader(false);
       }
     };
     fetchCurrentUser();
-  }, []);
+  }, [navigate]);
 
-  const handleClick = async () => {
-    setLoading(true);
-
+  const handleClick = useCallback(async () => {
+    setLoadingSave(true);
     try {
-      await saveUser(currentUser, false);
+      await saveUser(currentUser!, false);
       setMessage('User updated successfully');
       setSnackbarType('success');
-      setOpen(true);
-    } catch (error:any) {
+    } catch (error: any) {
       setSnackbarType('error');
       if (error.status === 401) {
-        setMessage('Session has expired navigating to login page');
+        setMessage('Session has expired. Redirecting to login page.');
         setTimeout(() => {
           navigate('/login');
         }, 2000);
       } else {
-        console.log(error);
-        setMessage('Error updating user');
+        console.error('Error updating user', error);
+        setMessage(error.message);
       }
     } finally {
       setTimeout(() => {
-        setLoading(false);
         setOpen(true);
-      }, 500);
+        setLoadingSave(false);
+      }, 700);
     }
-  };
+  }, [currentUser, navigate]);
 
-  const formatDate = (timestamp: number) => {
-    const date = new Date(timestamp);
-    return date.toLocaleString();
-  };
+  const formatDate = useCallback((timestamp: number) => {
+    return new Date(timestamp).toLocaleString();
+  }, []);
+
+  const handleInputChange = useCallback((key: keyof User, value: string) => {
+    setCurrentUser((prev) => (prev ? { ...prev, [key]: value } : prev));
+  }, []);
+
+  const userFields = useMemo(
+    () => [
+      { label: 'Email', value: currentUser?.email ?? '', key: 'email' },
+      {
+        label: 'First Name',
+        value: currentUser?.firstName ?? '',
+        key: 'firstName',
+      },
+      {
+        label: 'Last Name',
+        value: currentUser?.lastName ?? '',
+        key: 'lastName',
+      },
+      { label: 'Phone Number', value: currentUser?.phone ?? '', key: 'phone' },
+      {
+        label: 'Authority',
+        value: currentUser?.authority ?? '',
+        key: 'authority',
+      },
+    ],
+    [currentUser]
+  );
 
   return (
     <>
@@ -83,108 +106,69 @@ const Accountinfo = () => {
         {loader ? (
           <Loader />
         ) : (
-          <>
-            <div className="accountinfo">
-              <header className="accountinfo-header">
-                <div className="accountinfo-email">
-                  <p className="accountinfo-profile">Profile</p>
-                </div>
-                <div className="accountinfo-lastlogin">
-                  <p>Last Login</p>
-                  <p>
-                    {formatDate(
-                      currentUser?.additionalInfo?.lastLoginTs || Date.now()
-                    )}
-                  </p>
-                </div>
-              </header>
-              <main className="accountinfo-main">
-                {[
-                  'Email',
-                  'First Name',
-                  'Last Name',
-                  'Phone Number',
-                  'Authority',
-                ].map((label, index) => (
-                  <Box
-                    key={index}
+          <div className="accountinfo">
+            <header className="accountinfo-header">
+              <div className="accountinfo-email">
+                <p className="accountinfo-profile">Profile</p>
+              </div>
+              <div className="accountinfo-lastlogin">
+                <p>Last Login</p>
+                <p>
+                  {formatDate(
+                    currentUser?.additionalInfo?.lastLoginTs || Date.now()
+                  )}
+                </p>
+              </div>
+            </header>
+            <main className="accountinfo-main">
+              {userFields.map((field, index) => (
+                <Box
+                  key={index}
+                  sx={{
+                    width: '100%',
+                    maxWidth: '100%',
+                    backgroundColor: '#ebebeb',
+                    marginBottom: '10px',
+                  }}
+                >
+                  <TextField
+                    fullWidth
+                    label={field.label}
+                    value={field.value}
+                    onChange={(e) =>
+                      handleInputChange(field.key as keyof User, e.target.value)
+                    }
+                    disabled={field.label === 'Authority'}
                     sx={{
-                      width: '100%',
-                      maxWidth: '100%',
-                      backgroundColor: '#ebebeb',
-                      marginBottom: '10px',
+                      '& .MuiInputBase-input.Mui-disabled': {
+                        cursor:
+                          field.label === 'Authority' ? 'not-allowed' : 'auto',
+                      },
+                      '&:hover .MuiInputBase-input.Mui-disabled': {
+                        cursor:
+                          field.label === 'Authority' ? 'not-allowed' : 'auto',
+                      },
                     }}
-                  >
-                    <TextField
-                      fullWidth
-                      label={label}
-                      value={
-                        label === 'Email'
-                          ? currentUser.email
-                          : label === 'First Name'
-                          ? currentUser.firstName
-                          : label === 'Last Name'
-                          ? currentUser.lastName
-                          : label === 'Phone Number'
-                          ? currentUser.phone
-                          : label === 'Authority'
-                          ? currentUser.authority
-                          : ''
-                      }
-                      onChange={(e) => {
-                        if (label === 'Email')
-                          setCurrentUser((prev) => ({
-                            ...prev,
-                            email: e.target.value,
-                          }));
-                        if (label === 'First Name')
-                          setCurrentUser((prev) => ({
-                            ...prev,
-                            firstName: e.target.value,
-                          }));
-                        if (label === 'Last Name')
-                          setCurrentUser((prev) => ({
-                            ...prev,
-                            lastName: e.target.value,
-                          }));
-                        if (label === 'Phone Number')
-                          setCurrentUser((prev) => ({
-                            ...prev,
-                            phone: e.target.value,
-                          }));
-                      }}
-                      disabled={label === 'Authority'}
-                      sx={{
-                        '& .MuiInputBase-input.Mui-disabled': {
-                          cursor:
-                            label === 'Authority' ? 'not-allowed' : 'auto',
-                        },
-                        '&:hover .MuiInputBase-input.Mui-disabled': {
-                          cursor:
-                            label === 'Authority' ? 'not-allowed' : 'auto',
-                        },
-                      }}
-                    />
-                  </Box>
-                ))}
-                <div className="accountinfo-savebtn">
-                  <LoadingButton
-                    size="small"
-                    color="secondary"
-                    onClick={handleClick}
-                    loading={loading}
-                    loadingPosition="start"
-                    startIcon={<SaveIcon />}
-                    variant="contained"
-                    disabled={loading}
-                    sx={{ width: '150px', height: '50px' }}
-                  >
-                    <span>Save</span>
-                  </LoadingButton>
-                </div>
-              </main>
-            </div>
-          </>
+                  />
+                </Box>
+              ))}
+              <div className="accountinfo-savebtn">
+                <LoadingButton
+                  size="small"
+                  color="secondary"
+                  onClick={handleClick}
+                  loading={loadingSave}
+                  loadingPosition="start"
+                  startIcon={<SaveIcon />}
+                  variant="contained"
+                  disabled={loadingSave}
+                  sx={{ width: '150px', height: '50px' }}
+                >
+                  <span>Save</span>
+                </LoadingButton>
+              </div>
+            </main>
+          </div>
         )}
       </div>
 
