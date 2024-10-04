1974bf536f6cdaad743f3be71510da16108e043f
Niyaj Kumanali <niyajkumanali@gmail.com>
2024-10-04 09:39:00 +0530

    updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [
     react(),
     inspect(),
-  ],
-  optimizeDeps: {
-    include: ['react', 'react-dom'], // Include essential dependencies
-  },
+  ]
 });
